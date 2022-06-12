b9820e19ec9a5b21aa19d030595d31a932259d28
Kel Onyewuenyi <95924181+Kel-KO@users.noreply.github.com>
2022-06-12 15:40:23 -0500

    Update app.py

--- TTH_Inventory/app.py
+++ TTH_Inventory/app.py
@@ -27,14 +27,30 @@ def retail():
         # display retail page
         return render_template("rt.html")
 
-@app.route("/admissions",  methods=["GET", "POST"])
+@app.route("/admissionsadmin",  methods=["GET", "POST"])
 def admissions():
     if request.method == "GET":
         # display admissions page
         return render_template("ad.html")
+        
 
-@app.route("/foodandbevadmin",  methods=["GET", "POST"])
+@app.route("/foodandbev",  methods=["GET", "POST"])
 def foodandbev():
     if request.method == "GET":
         # display food and bev page
-        return render_template("fbadmin.html")
+        x = True
+        # if (request.form.get("pass")) == "Typh00n2022":
+            # x = True
+        if x == True:
+            return render_template("fbadmin.html")
+        else:
+            return render_template("fb.html")
+    else:
+        item = (request.form.get("item"))
+        quantity = int(request.form.get("quantity"))
+        location = (request.form.get("location"))
+        note = (request.form.get("note"))
+
+        # db.execute("UPDATE fbtable")
+
+        return render_template("fbadmin.html" )
